986d99669453742231d107c276820bc06efe12ac
josephC45 <joey.curtog@gmail.com>
2024-09-29 23:11:11 -0400

    initial commit

---
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/TrieMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.typeahead.trie_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrieMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrieMicroserviceApplication.class, args);
+	}
+
+}
---
+++ trie_microservice/src/test/java/com/typeahead/trie_microservice/TrieMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typeahead.trie_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrieMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

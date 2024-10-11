67fbe493b20f6dabca02e5f5c849ab855490f255
josephC45 <joey.curtog@gmail.com>
2024-10-10 21:11:18 -0400

    update declaration of instance variables, add overridden websockethandler methods for websocket/spring to utilize, improve readability of isEndOfWord and queryTrie methods, update class so that it is a spring managed bean

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/TrieWebsocketHandler.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/TrieWebsocketHandler.java
@@ -3,6 +3,8 @@ package com.typeahead.trie_microservice.websocket;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -10,20 +12,35 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import com.typeahead.trie_microservice.service.KafkaProducerService;
 import com.typeahead.trie_microservice.service.TrieService;
 
+@Component
 public class TrieWebsocketHandler extends TextWebSocketHandler {
 
-    private TrieService trieService;
-    private KafkaProducerService kafkaService;
-    private StringBuilder wordTyped = new StringBuilder();
+    private final TrieService trieService;
+    private final KafkaProducerService kafkaService;
+    private final StringBuilder wordTyped = new StringBuilder();
 
     public TrieWebsocketHandler(TrieService trieService, KafkaProducerService kafkaService){
         this.trieService = trieService;
         this.kafkaService = kafkaService;
     }
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("WebSocket connection established with client: " + session.getId());
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        System.err.println("Error in WebSocket transport for client: " + session.getId() + " - " + exception.getMessage());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("WebSocket connection closed for client: " + session.getId());
+    }
+
     private boolean isEndOfWord(String currentPrefix){
-        if(currentPrefix.matches("[^A-Za-z]")) return true;
-        return false;
+        return currentPrefix.matches("[^A-Za-z]");
     }
 
     private void sendResponseToClient(WebSocketSession session, TextMessage response){
@@ -34,9 +51,8 @@ public class TrieWebsocketHandler extends TextWebSocketHandler {
         }
     }
 
-    public void queryTrie(WebSocketSession session, TextMessage textMessage){
+    private void queryTrie(WebSocketSession session, String currentPrefix){
         //Idea to send prefix via kafka which will then be ingested by spark (microbatching)
-        String currentPrefix = textMessage.getPayload().toString();
         if(!isEndOfWord(currentPrefix)) {
             System.out.println("Querying trie with prefix: " + currentPrefix);
             List<String> popularAssociatedWordsWithPrefix = trieService.getPopularPrefixes(currentPrefix);
@@ -46,9 +62,17 @@ public class TrieWebsocketHandler extends TextWebSocketHandler {
                 : new TextMessage(String.join(",", popularAssociatedWordsWithPrefix));
 
             sendResponseToClient(session, response);
+            wordTyped.append(currentPrefix);
         }else{
             kafkaService.sendMessageToKafka(wordTyped.toString());
             wordTyped.setLength(0);
         }
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage textMessage){
+        String currentPrefix = textMessage.getPayload();
+        System.out.println("Received message: " + currentPrefix + " from client: " + session.getId());
+        queryTrie(session, currentPrefix);
     }
 }

b74a5ca4f703b5826a8c2b093d7b066b0c343e6c
josephC45 <joey.curtog@gmail.com>
2024-10-10 20:47:17 -0400

    update declaration for instance variable, update allowed origins for websocket, update dependencies

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/configuration/WebSocketConfig.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/configuration/WebSocketConfig.java
@@ -5,25 +5,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.typeahead.trie_microservice.service.KafkaProducerService;
-import com.typeahead.trie_microservice.service.TrieService;
 import com.typeahead.trie_microservice.websocket.TrieWebsocketHandler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
 
-    private TrieService trieService;
-    private KafkaProducerService kafkaProducerService;
-
-    public WebSocketConfig(TrieService trieService, KafkaProducerService kafkaProducerService){
-        this.trieService = trieService;
-        this.kafkaProducerService = kafkaProducerService;
+    private final TrieWebsocketHandler trieWebsocketHandler;
+    
+    public WebSocketConfig(TrieWebsocketHandler trieWebsocketHandler){
+        this.trieWebsocketHandler = trieWebsocketHandler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new TrieWebsocketHandler(trieService, kafkaProducerService), "/ws").setAllowedOrigins("*");
+        registry.addHandler(trieWebsocketHandler, "/ws").setAllowedOrigins("http://localhost:8080");
     }
 
 }

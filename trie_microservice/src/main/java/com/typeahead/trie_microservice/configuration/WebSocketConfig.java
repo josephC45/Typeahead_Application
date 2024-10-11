002fb3d8de9f879a83fc9761f0ca2b67516a95c5
josephC45 <joey.curtog@gmail.com>
2024-10-10 21:05:49 -0400

    update dependency injection by injectng via reference to websockethandler interface compared to a concrete implementation

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/configuration/WebSocketConfig.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/configuration/WebSocketConfig.java
@@ -1,19 +1,18 @@
 package com.typeahead.trie_microservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import com.typeahead.trie_microservice.websocket.TrieWebsocketHandler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
 
-    private final TrieWebsocketHandler trieWebsocketHandler;
-    
-    public WebSocketConfig(TrieWebsocketHandler trieWebsocketHandler){
+    private final WebSocketHandler trieWebsocketHandler;
+
+    public WebSocketConfig(WebSocketHandler trieWebsocketHandler){
         this.trieWebsocketHandler = trieWebsocketHandler;
     }
 

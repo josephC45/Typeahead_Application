40f1d791dac5d3877f17081ce8cad2f7042e9a4c
josephC45 <joey.curtog@gmail.com>
2024-10-20 20:58:30 -0400

    remove import statement

--- trie_microservice/src/test/java/com/typeahead/trie_microservice/domain/TrieTests.java
+++ trie_microservice/src/test/java/com/typeahead/trie_microservice/domain/TrieTests.java
@@ -3,7 +3,6 @@ package com.typeahead.trie_microservice.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.contains;
 
 import java.util.List;
 

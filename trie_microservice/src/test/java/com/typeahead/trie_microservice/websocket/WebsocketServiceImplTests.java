7c5242a586cbf129e1fcc6eea4242e30a1c9014f
josephC45 <joey.curtog@gmail.com>
2024-10-21 21:52:53 -0400

    refactor WebsocketServiceImplTests to reflect changes made to WebsocketServiceImpl class

--- trie_microservice/src/test/java/com/typeahead/trie_microservice/websocket/WebsocketServiceImplTests.java
+++ trie_microservice/src/test/java/com/typeahead/trie_microservice/websocket/WebsocketServiceImplTests.java
@@ -1,13 +1,15 @@
 package com.typeahead.trie_microservice.websocket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -35,17 +37,43 @@ public class WebsocketServiceImplTests {
     @InjectMocks
     private WebsocketServiceImpl websocketService;
 
+    private StringBuilder wordTyped;
+
+    @BeforeEach()
+    public void setup(){
+        wordTyped = new StringBuilder();
+    }
+
     @Test
     void givenValidPrefix_whenWebsocketQueriesTrie_shouldReturnPopularPrefixesToClient() throws IOException {
         List<String> mockPrefixes = Arrays.asList("testing", "testosterone");
-        when(trieService.getPopularPrefixes("test")).thenReturn(mockPrefixes);
+        lenient().when(trieService.getPopularPrefixes("test")).thenReturn(mockPrefixes);
 
-        websocketService.queryTrie(session, "test");
+        // Simulate typing characters to form the word "test"
+        websocketService.queryTrie(session, "t");
+        websocketService.queryTrie(session, "e");
+        websocketService.queryTrie(session, "s");
+        websocketService.queryTrie(session, "t");
+
         verify(trieService).getPopularPrefixes("test");
 
         ArgumentCaptor<TextMessage> captor = ArgumentCaptor.forClass(TextMessage.class);
-        verify(session).sendMessage(captor.capture());
-        TextMessage sentMessage = captor.getValue();
+        verify(session, atLeastOnce()).sendMessage(captor.capture());
+        TextMessage sentMessage = captor.getAllValues().get(captor.getAllValues().size() - 1);
         assertEquals("testing,testosterone", sentMessage.getPayload());
+    }
+
+    @Test
+    void whenEndOfWordCharacterIsTyped_shouldSendWordToKafka() throws Exception{
+        websocketService.queryTrie(session, "t");
+        websocketService.queryTrie(session, "e");
+        websocketService.queryTrie(session, "s");
+        websocketService.queryTrie(session, "t");
+        
+        // Simulate pressing a space (end of word)
+        websocketService.queryTrie(session, " ");
+        
+        verify(kafkaService).sendMessageToKafka("test");
+        assertEquals(0, wordTyped.length());
     }
 }

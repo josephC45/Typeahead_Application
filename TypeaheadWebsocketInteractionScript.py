c09f68df5a4b78ff2c4ab6b979b4ec1230b06bc9
josephC45 <joey.curtog@gmail.com>
2024-10-22 21:18:59 -0400

    refactor websocketinteraction script to allow for cross-platform support, real-time interaction via reading each keystroke, and concurrency

--- TypeaheadWebsocketInteractionScript.py
+++ TypeaheadWebsocketInteractionScript.py
@@ -1,13 +1,33 @@
 import asyncio
 import websockets
+import keyboard
+
+async def send_keyboard_input(websocket):
+    print("Type a prefix (press ESC to exit): ")
+    while(True):
+        key_event = keyboard.read_event()
+
+        if(key_event.event_type == keyboard.KEY_DOWN):
+            if(key_event.name == "esc"):
+                print("\nExiting websocket...")
+                break;
+            
+            await websocket.send(key_event.name)
+
+async def receive_suggestions(websocket):
+    try:
+        while(True):
+            popular_suggestions = await websocket.recv()
+            print(f"\nPopular suggestions: {popular_suggestions}")
+    except websockets.ConnectionClosed:
+        print("Websocket connection closed.")
 
 async def interact_with_websocket():
     uri = 'ws://localhost:8080/ws'
     async with websockets.connect(uri) as websocket:
-        while True:
-            prefix = input("Type a prefix: ")
-            await websocket.send(prefix)
-            popularSuggestions = await websocket.recv()
-            print(f"Popular suggestions: {popularSuggestions}")
+        await asyncio.gather(
+            send_keyboard_input(websocket),
+            receive_suggestions(websocket)
+        )
 
-asyncio.get_event_loop().run_until_complete(interact_with_websocket())
+asyncio.run(interact_with_websocket())

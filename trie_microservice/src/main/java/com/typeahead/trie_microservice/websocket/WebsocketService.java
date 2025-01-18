ddc7f5db3d9da64a65d6cd68e37b4e7135452c55
josephC45 <joey.curtog@gmail.com>
2025-01-18 16:02:14 -0500

    change method signature to return Mono<String>

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/WebsocketService.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/WebsocketService.java
@@ -3,7 +3,7 @@ package com.typeahead.trie_microservice.websocket;
 import reactor.core.publisher.Mono;
 
 public interface WebsocketService {
-    
+
     public Mono<String> queryTrie(String currentPrefix);
-    
+
 }

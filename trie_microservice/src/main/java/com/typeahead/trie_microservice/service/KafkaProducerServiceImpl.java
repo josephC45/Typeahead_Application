c9f5e924858fb111aa2c84d53f35ae3c143e208d
josephC45 <joey.curtog@gmail.com>
2025-01-18 16:00:20 -0500

    change method signatures to utilize Mono, change sendMessageToKafka method to be reactive

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/service/KafkaProducerServiceImpl.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/service/KafkaProducerServiceImpl.java
@@ -11,6 +11,7 @@ import com.typeahead.trie_microservice.infrastructure.KafkaProducerService;
 
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
+import reactor.core.publisher.Mono;
 
 @Service
 public class KafkaProducerServiceImpl implements KafkaProducerService {
@@ -24,34 +25,39 @@ public class KafkaProducerServiceImpl implements KafkaProducerService {
         this.tracer = tracer;
     }
 
-    public void sendMessageToKafka(String prefix) {
-        Span span = tracer.nextSpan().name("kafka-producer").start();
-        try {
-            tracer.withSpan(span);
-            Span currentSpan = tracer.currentSpan();
-            if (currentSpan != null) {
-                ProducerRecord<String, String> record = createKafkaRecord(prefix, currentSpan);
-                kafkaTemplate.send(record);
-            } else {
-                logger.warn("No active trace context found, sending message without tracing.");
-                kafkaTemplate.sendDefault(prefix);
-            }
-        } catch (KafkaException e) {
-            logger.error("KafkaTemplate sendDefault ran into error sending prefix to Kafka: {}", e.getMessage(), e);
-            throw e;
-        } finally {
-            span.end();
-        }
-    }
-
-    private ProducerRecord<String, String> createKafkaRecord(String prefix, Span currentSpan) {
+    private Mono<ProducerRecord<String, String>> createKafkaRecord(String prefix, Span currentSpan) {
         ProducerRecord<String, String> record = new ProducerRecord<>(kafkaTemplate.getDefaultTopic(), prefix);
 
         record.headers().add("trace-id", currentSpan.context().traceId().getBytes());
         record.headers().add("span-id", currentSpan.context().spanId().getBytes());
         record.headers().add("parent-span-id", currentSpan.context().parentId().getBytes());
 
-        return record;
+        return Mono.just(record);
     }
 
+    public Mono<Void> sendMessageToKafka(String prefix) {
+        Span span = tracer.nextSpan().name("kafka-producer").start();
+
+        return Mono.defer(() -> {
+                return Mono.justOrEmpty(tracer.currentSpan())
+                .flatMap(currentSpan -> {
+                    if (currentSpan != null) {
+                        return createKafkaRecord(prefix, currentSpan)
+                                .flatMap(record -> {
+                                    return Mono.fromRunnable(() -> kafkaTemplate.send(record));
+                                });
+                    } else {
+                        logger.warn("No active trace context found, sending message without tracing.");
+                        return Mono.fromRunnable(() -> kafkaTemplate.sendDefault(prefix));
+                    }
+
+                })
+                .onErrorResume(KafkaException.class, e -> {
+                    logger.error("KafkaTemplate sendDefault ran into error sending prefix to Kafka: {}", e.getMessage(), e);
+                    return Mono.empty();
+                })
+                .doOnTerminate(() -> span.end())
+                .then();
+        });
+    }
 }

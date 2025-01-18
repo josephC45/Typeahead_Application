ae743d1123f657cbc9caa36a3532decd2a5f196d
josephC45 <joey.curtog@gmail.com>
2025-01-18 14:57:30 -0500

    remove sendMessageToClient method signature and change method signature of queryTrie for reactive programming changes

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/WebsocketService.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/websocket/WebsocketService.java
@@ -1,14 +1,9 @@
 package com.typeahead.trie_microservice.websocket;
 
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
 
 public interface WebsocketService {
     
-    public boolean isEndOfWord(String currentPrefix);
-
-    public void sendResponseToClient(WebSocketSession session, TextMessage textMessage);
-
-    public void queryTrie(WebSocketSession session, String currentPrefix);
+    public Mono<String> queryTrie(String currentPrefix);
     
 }

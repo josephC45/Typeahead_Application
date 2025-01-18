71ca990f615f2175f89e6014a5c6acc50e1eb6dd
josephC45 <joey.curtog@gmail.com>
2025-01-17 19:57:43 -0500

    refactor getPrefixes method to return Flux, create dfsConstructPopularWords method to construct popular words with a given prefix (space efficient) rather than storing lists of words at each node of the trie

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/domain/TrieImpl.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/domain/TrieImpl.java
@@ -1,13 +1,15 @@
 package com.typeahead.trie_microservice.domain;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.typeahead.trie_microservice.exception.TrieException;
+
+import reactor.core.publisher.Flux;
 
 @Component
 public class TrieImpl implements TrieInterface {
@@ -19,20 +21,41 @@ public class TrieImpl implements TrieInterface {
         this.rootNode = new TrieNode();
     }
 
-    public List<String> getPrefixes(String prefix) {
+    private Flux<String> dfsConstructPopularWords(TrieNode node, String prefix) {
+        return Flux.create(sink -> {
+            Stack<Map.Entry<TrieNode, String>> stack = new Stack<>();
+            stack.push(Map.entry(node, prefix));
+            while (!stack.isEmpty()) {
+                Map.Entry<TrieNode, String> current = stack.pop();
+                TrieNode currentNode = current.getKey();
+                String currentPrefix = current.getValue();
+
+                if (currentNode.isEndOfWord())
+                    sink.next(currentPrefix);
+
+                currentNode.getChildren()
+                        .entrySet()
+                        .forEach(child -> stack.push(Map.entry(child.getValue(), currentPrefix + child.getKey())));
+            }
+
+            sink.complete();
+        });
+    }
+
+    @Override
+    public Flux<String> getPrefixes(String prefix) {
         TrieNode currentNode = rootNode;
         for (char currentCharacter : prefix.toCharArray()) {
             currentNode = currentNode.getChildren().get(currentCharacter);
             if (currentNode == null)
-                return Collections.emptyList();
+                return Flux.empty();
         }
-        return (currentNode.getMostPopularWordsWithPrefix() != null)
-                ? currentNode.getMostPopularWordsWithPrefix()
-                : Collections.emptyList();
+        return dfsConstructPopularWords(currentNode, prefix);
     }
 
     // Error handling put in place is temporary until HBase is
     // implemented and I determine how I need to handle it
+    @Override
     public void addPrefix(String prefix) {
         try {
             if (prefix == null || prefix.isEmpty()) {
@@ -42,13 +65,13 @@ public class TrieImpl implements TrieInterface {
             TrieNode currentNode = rootNode;
             for (char currentCharacter : prefix.toCharArray()) {
                 currentNode = currentNode.addChildIfAbsent(currentCharacter);
-                currentNode.addToPopularWords(prefix);
             }
             currentNode.setEndOfWord(true);
 
         } catch (TrieException e) {
             logger.error("Error while adding prefixes: {}", e.getMessage());
-            throw e; //revisit how we are testing/handling this exception
+            throw e; // revisit how we are testing/handling this exception
         }
     }
+
 }

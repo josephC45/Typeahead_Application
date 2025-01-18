ded3939641bab4fdb9c227866ef5cc0628c89215
josephC45 <joey.curtog@gmail.com>
2025-01-17 19:48:45 -0500

    remove popular prefixes list field

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/domain/TrieNode.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/domain/TrieNode.java
@@ -1,28 +1,16 @@
 package com.typeahead.trie_microservice.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TrieNode {
 
-    private final List<String> mostPopularWordsWithPrefix;
     private final Map<Character, TrieNode> children;
     private boolean isEndOfWord;
 
     public TrieNode() {
         this.isEndOfWord = false;
         this.children = new HashMap<>();
-        this.mostPopularWordsWithPrefix = new ArrayList<>();
-    }
-
-    List<String> getMostPopularWordsWithPrefix() {
-        return mostPopularWordsWithPrefix;
-    }
-
-    void addToPopularWords(String prefix) {
-        this.mostPopularWordsWithPrefix.add(prefix);
     }
 
     boolean isEndOfWord() {

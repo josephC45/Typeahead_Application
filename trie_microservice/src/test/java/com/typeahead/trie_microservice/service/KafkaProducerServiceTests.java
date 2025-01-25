00cb4e0864af67355f1d663bc743c7a71c5d5469
josephC45 <joey.curtog@gmail.com>
2025-01-24 20:36:09 -0500

    change tests for KafkaProducerServiceImpl for reactive code changes

--- trie_microservice/src/test/java/com/typeahead/trie_microservice/service/KafkaProducerServiceTests.java
+++ trie_microservice/src/test/java/com/typeahead/trie_microservice/service/KafkaProducerServiceTests.java
@@ -1,9 +1,8 @@
 package com.typeahead.trie_microservice.service;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +12,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import com.typeahead.trie_microservice.exception.KafkaException;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 public class KafkaProducerServiceTests {
@@ -25,14 +27,28 @@ public class KafkaProducerServiceTests {
 
     @Test
     void whenSendMessageToKafkaCalled_shouldSendMessageSuccessfully() {
-        kafkaService.sendMessageToKafka("test");
-        verify(kafkaTemplateMock, times(1)).sendDefault("test");
+        String prefix = "test";
+        when(kafkaTemplateMock.sendDefault(prefix)).thenReturn(null);
+
+        Mono<Void> result = kafkaService.sendMessageToKafka(prefix);
+
+        StepVerifier.create(result).verifyComplete();
+
+        verify(kafkaTemplateMock, times(1)).sendDefault(prefix);
     }
 
     @Test
     void whenSendMessageToKafkaThrowsKafkaException_shouldThrowException() {
-        doThrow(new KafkaException("Kafka exception occurred")).when(kafkaTemplateMock).sendDefault("test");
-        assertThrows(KafkaException.class, () -> kafkaService.sendMessageToKafka("test"));
-        verify(kafkaTemplateMock, times(1)).sendDefault("test");
+        String prefix = "test";
+        when(kafkaTemplateMock.sendDefault(prefix)).thenThrow(KafkaException.class);
+
+        Mono<Void> result = kafkaService.sendMessageToKafka(prefix);
+
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable -> throwable instanceof KafkaException
+                        && throwable.getMessage().equals("Kafka message sending failed"))
+                .verify();
+
+        verify(kafkaTemplateMock, times(1)).sendDefault(prefix);
     }
 }

a5ab533192926cc32c0cae0be80567c089704d4d
josephC45 <joey.curtog@gmail.com>
2025-02-22 21:43:54 -0500

    refactor to implement Trie interface and specify qualifier for dependency injection

--- trie_microservice/src/main/java/com/typeahead/trie_microservice/service/TrieServiceImpl.java
+++ trie_microservice/src/main/java/com/typeahead/trie_microservice/service/TrieServiceImpl.java
@@ -2,29 +2,28 @@ package com.typeahead.trie_microservice.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.typeahead.trie_microservice.domain.TrieInterface;
-import com.typeahead.trie_microservice.domain.TrieService;
-
+import com.typeahead.trie_microservice.domain.Trie;
 import reactor.core.publisher.Mono;
 
 @Service
-public class TrieServiceImpl implements TrieService {
+public class TrieServiceImpl implements Trie {
 
-    private TrieInterface trie;
+    private Trie trie;
 
-    public TrieServiceImpl(TrieInterface trie) {
+    public TrieServiceImpl(@Qualifier("trieImpl") Trie trie) {
         this.trie = trie;
     }
 
     @Override
-    public Mono<List<String>> getPopularPrefixes(String prefix) {
+    public Mono<List<String>> getPrefixes(String prefix) {
         return trie.getPrefixes(prefix);
     }
 
     @Override
-    public void addCurrentPrefix(String prefix) {
+    public void addPrefix(String prefix) {
         trie.addPrefix(prefix);
     }
 
